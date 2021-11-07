ff594c919a0704fe804860dcaa4962ea2a0fd1d9
JaseZiv <jaseziv83@gmail.com>
2021-11-07 14:34:24 +1100

    create job for match results

---
+++ raw-data/big-five-match-results/get_match_results.R
@@ -0,0 +1,45 @@
+library(worldfootballR)
+library(tidyverse)
+library(here)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Past Seasons ------------------------------------------------------------
+
+# ***** NOTE: *********************************************
+# This section doesn't need to be run again - the results 
+# are already stored (top5_league_results_to_2021.rds)
+# *********************************************************
+
+# # first run to get past full season's data
+# results <- get_match_results(country = c("ENG", "ESP", "FRA", "GER", "ITA"), gender = "M", season_end_year = c(2018:2021))
+# 
+# # filter out any playoffs, abandoned games, etc
+# results <- results %>% filter(!is.na(Wk),
+#                               !is.na(HomeGoals))
+# 
+# # save original data
+# saveRDS(results, here("raw-data", "big-five-match-results", "top5_league_results_to_2021.rds"))
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Updating Data -----------------------------------------------------------
+
+# reda in the old data
+results <- readRDS(here("raw-data", "big-five-match-results", "top5_league_results_to_2021.rds"))
+
+# get new data
+newdata <- get_match_results(country = c("ENG", "ESP", "FRA", "GER", "ITA"), gender = "M", season_end_year = 2022)
+
+# filter out any playoffs, abandoned games, yet-to-be-played games, etc
+newdata <- newdata %>%
+  filter(!is.na(Wk),
+         !is.na(HomeGoals))
+
+# join on to new data
+results <- results %>%
+  bind_rows(newdata)
+
+
+# save up-to-date data
+saveRDS(results, here("raw-data", "big-five-match-results", "top5_league_results.rds"))
+
--- raw-data/job_controller.R
+++ raw-data/job_controller.R
@@ -5,3 +5,6 @@ library(here)
 
 # Scrape All Competition Season’s Data ------------------------------------
 source(here::here("raw-data", "big-5-stats-and-values", "update_fbref_data.R"))
+
+# Scrape Big Five League Match Results ------------------------------------
+source(here::here("raw-data", "big-five-match-results", "get_match_results.R"))
